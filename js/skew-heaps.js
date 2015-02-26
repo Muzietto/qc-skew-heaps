27df8d4461a353aa4298eebe8157965b02320d2f
Stefano Sarioli <stefano.sarioli@gmail.com>
2015-02-26 16:53:08 +0100

    more operation, added utility library and jscheck test

--- js/skew-heaps.js
+++ js/skew-heaps.js
@@ -1,4 +1,3 @@
-
 function fork(label,left,right) {
   return function(w) {
     return w(label,left,right);
@@ -10,8 +9,8 @@ function left(x,l,r){return l;}
 function right(x,l,r){return r;}
 
 function insert(x,tree) {
-  if (tree === null) return fork(x,null,null)
-  return fork(Math.min(x,tree(label)),tree(right),insert(Math.max(x,tree(label)),tree(left)))
+  if (tree === null) return fork(x,null,null);
+  return fork(Math.min(x,tree(label)),tree(right),insert(Math.max(x,tree(label)),tree(left)));
 }
 
 function make(array){
@@ -20,4 +19,77 @@ function make(array){
     return insert(curr,acc)
   },null)
 }
+
+function makeWhitPattern(elements, ops){
+	if (elements.length === 0) return null;
+	pattern = _.zip(ops, elements);
+    return pattern.reduce(function(acc,curr){
+	  if(curr[0] === insert) return curr[0](curr[1],acc);
+	  return curr[0](acc);
+    },null);		
+}
+
+function invariant(tree){
+	if (tree === null) return true;
+	return smaller(tree(label), tree(left)) && smaller(tree(label), tree(right));
+}
+
+function smaller(x, tree){
+	if (tree === null) return true;
+	return (x <= tree(label)) && invariant(tree);
+}
+
+function minElem(tree){
+	if (tree === null) return null;
+	return tree(label);
+}
+
+function d(tree){
+	return weight(tree(right)) - weight(tree(left));
+}
+
+function weight(tree){
+	if (tree === null) return 0;
+	return 1 + weight(tree(left)) + weight(tree(right));
+}
+
+function balanced(tree){
+	if (tree === null) return true;
+	return ((d(tree) === 0) || (d(tree) === 1)) && balanced(tree(left)) && balanced(tree(right));
+}
+
+function deleteMin(tree){
+	if (tree === null) return null;
+	return merge(tree(left), tree(right));
+}
+
+function merge(l, r){
+	if ((l === null) && (r === null)) return null;
+	if ((l === null)) return r;
+	if ((r === null)) return l;
+	if(minElem(l) <= minElem(r)) return join(l, r);
+	return join(r, l);
+}
+
+function join(tree1, tree2){
+	return fork(tree1(label), tree1(right), merge(tree1(left), tree2));	
+}
+
+function good(tree){
+	return (weight(tree(left)) <= weight(tree(right)));
+}
+
+function credits(tree){
+    if (tree === null) return 0;
+    return credits(tree(left)) + credits(tree(right)) + if (good(tree)) 0 else 1;
+}
+
+
+
+
+
+
+
+
+
 
--- js/skew-heaps_test.js
+++ js/skew-heaps_test.js
@@ -40,5 +40,112 @@ describe("a skew heap", function () {
     });
     
   })
+  
+  describe("returning is minimum element", function () {
+
+    it('that is null if the tree is empty', function () {
+      var arr = [];
+      var res = make(arr);
+      expect(minElem(res)).to.be.null;
+    });
+	
+	it('that is the minimum element if the tree is not empty', function () {
+      var arr = [36, 48, 56, 12];
+      var res = make(arr);
+      expect(minElem(res)).to.be.equal(12);
+    });
+    
+  })
+  
+  describe("return is weight", function (){
+	  
+	it('that is 0 if the tree is empty', function() {
+	  var arr = [];
+	  var res = make(arr);
+	  expect(weight(res)).to.be.equal(0);
+	});
+	
+	it('that is equal to the number of element in the tree', function() {
+		var arr = [59, 21, 23, 26];
+		var res = make(arr);
+		expect(weight(res)).to.be.equal(4);
+	});	  
+	  
+  })
+  
+  describe("has to be balanced", function (){
+	  
+	it('if is empty', function() {
+	  var arr = [];
+	  var res = make(arr);
+	  expect(balanced(res)).to.be.true;
+	});
+	
+	it('of populated', function() {
+	  var arr = [59, 21, 23, 26];
+	  var res = make(arr);
+	  expect(balanced(res)).to.be.true;
+	});	  
+	  
+  })  
+  
+  describe("built whit makeWhitPattern", function (){
+	  
+	it('if is empty', function() {
+	  var arr1 = [];
+	  var arr2 = [];
+	  var res = makeWhitPattern(arr1, arr2);
+	  expect(res).to.be.null;
+	});
+	
+	it('of populated', function() {
+	  var arr1 = [59, 21, 23, 21];
+	  var arr2 = [insert, insert, insert, deleteMin];
+	  var res = makeWhitPattern(arr1, arr2);
+	  expect(balanced(res)).to.be.true;
+	});	  
+    
+	it('trying credits', function() {
+	  var arr = [59, 21, 23, 26];
+	  var res = make(arr);
+	  expect(balanced(res)).to.be.true;
+	});
+	  
+  })  
 });
 
+JSC.on_report(function(str){console.log(str);});
+
+function test_invariant(verdict, arr){
+	return verdict(invariant(make(arr)));
+}
+
+function test_balanced(verdict, arr){
+	return verdict(balanced(make(arr)));
+}
+
+JSC.claim('testing invariant', test_invariant, [JSC.array(JSC.integer(100), JSC.integer())]);
+JSC.claim('testing balanced', test_balanced, [JSC.array(JSC.integer(100), JSC.integer())]);
+
+function test_invariant_whit_deletion(verdict, elements, ops){
+	return verdict(invariant(makeWhitPattern(elements, ops)));
+}
+
+function test_balanced_whit_deletion(verdict, elements, ops){
+	return verdict(balanced(makeWhitPattern(elements, ops)));
+}
+
+JSC.claim('testing invariant whit deletion', test_invariant_whit_deletion, 
+	[JSC.array(100, JSC.integer()), JSC.array(100, JSC.one_of([JSC.literal(insert), JSC.literal(deleteMin)]))]);
+//JSC.claim('testing balanced whit deletion', test_balanced_whit_deletion, 
+//	[JSC.array(100, JSC.integer()), JSC.array(100, JSC.one_of([JSC.literal(insert), JSC.literal(deleteMin)]))]);
+
+function test_good_whit_deletion(verdict, elements, ops){
+	return verdict(good(makeWhitPattern(elements, ops)));
+}
+
+//JSC.claim('testing good whit deletion', test_good_whit_deletion, 
+//	[JSC.array(100, JSC.integer()), JSC.array(100, JSC.one_of([JSC.literal(insert), JSC.literal(deleteMin)]))]);
+
+JSC.reps(1000);
+JSC.check(1000);
