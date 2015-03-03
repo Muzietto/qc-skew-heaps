0cc006770f948a1283462382592c5553da2d20c0
Muzietto <marco_faustinelli@yahoo.com>
2015-03-03 09:10:45 +0100

    first decent ajax chain

--- js/ajax.js
+++ js/ajax.js
@@ -1,28 +1,4 @@
 
-var ajax = MONAD()
-  .lift('getUrl',getUrl);
-  
-var get = function(url){
-  return function(v){
-    console.log('invoking ' + url);
-    return $.getJSON(url, function (data) {
-        console.log('URL: ' + url + '; SUCCESS: ' + data);
-        return ajax({ acc: v.acc + ' --> ' + url, data: data });
-    })
-    .fail(function(error){console.log('FAIL: ' + error)});
-  }
-}
-
-var getUrl = function(){
-  return function(v){
-    var url = v.data.url;
-    console.log('invoking ' + url);
-    return $.getJSON(url, function(data){
-      return ajax({ acc: v.acc + ' --> ' + url, data: data });
-    });
-  }
-}
-
 var gett = function(url){
     console.log('invoking ' + url);
     return $.getJSON(url, function (data) {
@@ -33,35 +9,92 @@ var gett = function(url){
     .fail(function(error){console.log('FAIL: ' + error)});
 }
 
+var getUrlFromData = function(v){
+  var url = v.data.url;
+  console.log('invoking ' + url);
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    type: 'GET',
+    cache: true,
+    async: false,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('URL: ' + url + '; SUCCESS: ' + data);
+      jsonResult = data;
+    },
+    error: function(error){console.log('ERR: ' + error)}
+  })
+  return ajax({ acc: v.acc + ' --> ' + url, data: jsonResult });
+}
+
+var ajax = MONAD()
+  .lift('getUrl',getUrlFromData)
+  // monad.value will be a plain value
+  .lift_value('value', function (s) {
+      return s;
+  });
+  
+var get = function(url){
+  return function(v){
+    var jsonResult = null;
+    console.log('invoking ' + url);
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'GET',
+      cache: true,
+      async: false,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('URL: ' + url + '; SUCCESS: ' + data);
+        jsonResult = data;
+      },
+      error: function(error){console.log('ERR: ' + error)}
+    })
+    return ajax({ acc: v.acc + ' --> ' + url, data: jsonResult });
+  }
+}
+
+var getUrlFromChain = function(){
+  return function(v){
+    var url = v.data.url;
+    console.log('invoking ' + url);
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'GET',
+      cache: true,
+      async: false,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('URL: ' + url + '; SUCCESS: ' + data);
+        jsonResult = data;
+      },
+      error: function(error){console.log('ERR: ' + error)}
+    })
+    return ajax({ acc: v.acc + ' --> ' + url, data: jsonResult });
+  }
+}
+
 $(document).ready(function(){
-/*
-  var result0 = ajax({ acc: '', data: {}});
+
+  var result0 = ajax({ acc: 'START', data: {}});
 
   var result1 = result0
     .bind(get('remote/first.json'));
-*/
 
-  var result0 = gett('remote/first.json')
-                  .then(function(data){
-                    console.log('data is now' + JSON.stringify(data));
-                    gett(data.url)
-                    .then(function(data){
-                      console.log('data is now' + JSON.stringify(data));
-                      gett(data.url)
-                    })
-                  });
-
-
-/*
   var result2 = result1
-    .getUrl(); // gets second.json
+    .bind(getUrlFromChain());
 
-  var result3 = result2
-    .getUrl(); // gets third.json
-
-
-debugger;
-  alert('gggggg');    */
+  var result10 = ajax({ acc: 'START', data: {}})
+    .bind(get('remote/first.json'))
+    .getUrl() // gets second.json
+    .getUrl() // gets third.json
+  ;
+  
+//debugger;
+  alert(result10.value().acc);    
   
   
 /*
@@ -78,5 +111,20 @@ $.ajax({
       },
       error: function(error){console.log('ERR: ' + error)}
     })
+*/
+
+
+/* this bugger works
+  var resultXXX = gett('remote/first.json')
+                  .then(function(data){
+                    console.log('data is now' + JSON.stringify(data));
+                    gett(data.url)
+                    .then(function(data){
+                      console.log('data is now' + JSON.stringify(data));
+                      gett(data.url)
+                    })
+                  });
+
+
 */
 });
