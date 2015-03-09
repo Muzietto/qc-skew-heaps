779cfa7f1e8cf06a11ddcbe954f2b65adf320334
Stefano Sarioli <stefano.sarioli@gmail.com>
2015-03-09 15:05:32 +0100

    Setup ambient for async testing whit jscheck

--- js/ajaxTest.js
+++ js/ajaxTest.js
@@ -1,9 +1,5 @@
-function testAjax(verdict) {
-  return verdict(result3.value().jump === 3);
-}
-
-function testThirdValue(verdict, url){
-  return verdict(result3.value().data.value === "third_value");
+function testThirdValue(verdict){
+  return verdict(base().value().data.value === "third_value");
 }
 
 function testNavigate(verdict, url){
@@ -11,33 +7,15 @@ function testNavigate(verdict, url){
 }
 
 JSC.group("sinc");
-JSC.claim('test navigate path jump', testNavigate, [JSC.one_of(['remote/first.json', 'remote/second.json'])]);
+JSC.claim('test navigate path jump', testNavigate,
+ [JSC.one_of(['remote/first.json', 'remote/second.json'])]);
 
 JSC.group("asinc");
-JSC.claim('test complete path jump', testAjax, []);
-JSC.claim('test third value', testThirdValue, [JSC.one_of(['remote/first.json', 'remote/second.json'])]);
-
-function ex1(callback) {
-  JSC.check("sinc", 1000);
-  callback();
-}
-
-function ex2(callback) {
-  base('remote/first.json');
-  JSC.check("asinc", 1000);
-  callback();
-}
+JSC.claim('test third value', testThirdValue, []);
 
 $(document).ready(function(){
-  //JSC.check(1000);  
-  async.series([
-    function(callback) {
-      ex1(callback);
-    },
-    function(callback) {
-      ex2(callback);
-    }
-  ]);
+  JSC.reps(1);
+  JSC.check(1000); 
 });
 
 
