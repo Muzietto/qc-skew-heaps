27f937ab66ac8d8f48cd5d7e7e3e17f970743465
Stefano Sarioli <stefano.sarioli@gmail.com>
2015-03-10 14:39:47 +0100

    Adedd file for playing with promise

---
+++ js/promise.js
@@ -0,0 +1,109 @@
+// ASYNC stuff
+var gett = function(url){
+  console.log('invoking ' + url);
+  return $.getJSON(url, function (data) {
+      console.log('URL: ' + url + '; SUCCESS: ' + JSON.stringify(data));
+      data.filippo = 12;
+      return data;
+  })
+  .fail(function(error){console.log('FAIL: ' + error)});
+}
+
+// SYNC stuff
+var getUrlFromData = function(v){
+  var url = v.data.url;
+  console.log('invoking ' + url);
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    type: 'GET',
+    cache: true,
+    async: false,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('URL: ' + url + '; SUCCESS: ' + data);
+      jsonResult = data;
+    },
+    error: function(error){console.log('ERR: ' + error)}
+  })
+  return ajax({ acc: v.acc + ' --> ' + url, data: jsonResult });
+}
+
+var ajax = MONAD()
+  .lift('getUrl',getUrlFromData)
+  // monad.value will be a plain value
+  .lift_value('value', function (s) {
+      return s;
+  });
+  
+var get = function(url){
+  return function(v){
+    var jsonResult = null;
+    console.log('invoking ' + url);
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'GET',
+      cache: true,
+      async: false,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('URL: ' + url + '; SUCCESS: ' + data);
+        jsonResult = data;
+      },
+      error: function(error){console.log('ERR: ' + error)}
+    })
+    return ajax({ acc: v.acc + ' --> ' + url, data: jsonResult });
+  }
+}
+
+var getUrlFromChain = function(){
+  return function(v){
+    var url = v.data.url;
+    console.log('invoking ' + url);
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'GET',
+      cache: true,
+      async: false,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('URL: ' + url + '; SUCCESS: ' + data);
+        jsonResult = data;
+      },
+      error: function(error){console.log('ERR: ' + error)}
+    })
+    return ajax({ acc: v.acc + ' --> ' + url, data: jsonResult });
+  }
+}
+
+$(document).ready(function(){
+  /*
+  var result0 = ajax({ acc: 'START', data: {}});
+
+  var result1 = result0
+    .bind(get('remote/first.json'));
+
+  var result2 = result1
+    .bind(getUrlFromChain());
+
+  var result10 = ajax({ acc: 'START', data: {}})
+    .bind(get('remote/first.json'))
+    .getUrl() // gets second.json
+    .getUrl() // gets third.json
+  ;
+  alert(result10.value().acc);    
+  */
+  // promises --> callback hell
+  var resultXXX = gett('remote/first.json')
+    .then(function(data){
+      console.log('data is now' + JSON.stringify(data));
+      gett(data.url)
+      .then(function(data){
+        console.log('data is now' + JSON.stringify(data));
+        gett(data.url)
+        .then(function(data){alert(data.value)});
+      })
+    });
+});
---
+++ js/promiseTest.js
@@ -0,0 +1,12 @@
+/*
+function testThirdValue(verdict){
+  return verdict(result3.value().data.value === "third_value");
+}
+
+JSC.claim('test third value', testThirdValue, []);
+
+$(document).ready(function(){
+  JSC.reps(3);
+  JSC.check(); 
+});
+*/
